7bd8e28e20ad08c3272427d6d967541896b5726a
Prince Wilfie Verame <princewilfieverame@gmail.com>
2024-04-04 03:24:29 +0800

    third commit

--- _helpers/send-email.js
+++ _helpers/send-email.js
@@ -1,11 +1,9 @@
 const nodemailer = require('nodemailer');
-const config = require('config.json');
-
+const config = require('./config.json');
 
 module.exports = sendEmail;
 
-
-async function sendEmail({ to, subject, html, from = config.emailFrom}){
-    const transporter = nodemailer.createTransport(config.smtOptions);
-    await transporter.sendMail({ from, to, subject, html});
+async function sendEmail({ to, subject, html, from = config.emailFrom }) {
+    const transporter = nodemailer.createTransport(config.smtpOptions);
+    await transporter.sendMail({ from, to, subject, html });
 }
